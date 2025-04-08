9fa72342c01718e5ca732d836e56e15091f5bb2c
Raja Mahin Zaman <62209180+RajaMahin@users.noreply.github.com>
2025-04-08 22:02:39 +0500

    Implementing Tasks-Component-Self

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { DUMMY_USERS } from './dummy-users';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { AppTasksComponent } from "./app-tasks/app-tasks.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, AppTasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -16,6 +17,7 @@ export class AppComponent {
 
   users = DUMMY_USERS;
 
+  taskName = "MY TASK"
 
   onSelectUser(id: string) {
     console.log("USER ID -> " + " " + id);
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -27,8 +27,8 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter();
 
+  @Output() select = new EventEmitter<string>();
   // id = input.required<string>();
   // avatar = input.required<string>();
   // name = input.required<string>();
